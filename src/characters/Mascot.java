b8ac2fdd4b434ec701ef2dddc723e2df1bb8538b
reg3x <diegoaguilera85@gmail.com>
2016-05-07 16:13:48 -0400

    adds level and mascot classes

---
+++ src/characters/Mascot.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package characters;
+
+/**
+ *
+ * @author diegoaguilerazambrano
+ */
+public class Mascot extends GameObject {
+    
+    public Mascot(String urlstring, int xInit, int yInit) {
+        super(urlstring, xInit, yInit);
+    }
+    
+}
