3f72bdd0f0f5bdc4cd931986166ed7cf5dae9944
reg3x <diegoaguilera85@gmail.com>
2016-05-07 23:04:15 -0400

    adds methods to create cars on a map and coordinates for roads

--- src/characters/Map.java
+++ src/characters/Map.java
@@ -5,16 +5,69 @@
  */
 package characters;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author reg3x
  */
 public class Map extends GameObject{
     private int level;
+    private int numRoads;
+    private int limitLeft, limitRight, limitUp;
+    private ArrayList<Integer> YRoadsCoordinates;
+    private ArrayList<Vehicle> vehicles;
    
-    public Map(String urlstring, int xInit, int yInit, int level) {
+    public Map(int level, String urlstring, int xInit, int yInit) {
         super(urlstring, xInit, yInit);
         this.level = level;
+        loadRoadsCoordinates();
+        createCars();
+    }
+    
+    public void createCars(){
+//        vehicles.add(new Vehicle("carro.png",0, 590));
+//        vehicles.add(new Vehicle("camion.png",650, 555));
+//        vehicles.add(new Vehicle("ambulancia.png",0, 500));
+//        vehicles.add(new Vehicle("limo.png",650, 340));
+        
+        // load cars evenly comming from left and right
+        vehicles = new ArrayList<Vehicle>();
+        for(int i=0;i<numRoads;i++){
+            if((i%2)==0){
+                vehicles.add(new Vehicle("carro.png", "MoveLeft",limitRight, YRoadsCoordinates.get(i), limitLeft));
+            }
+            else{
+                vehicles.add(new Vehicle("carro.png", "MoveRight",limitLeft, YRoadsCoordinates.get(i), limitRight));
+            }
+        }
+        
+    }
+    
+    public void loadRoadsCoordinates(){
+        YRoadsCoordinates = new ArrayList<>();
+        switch(level){
+            case 1:
+                numRoads=4;
+                limitLeft = 0;
+                limitRight = 650;
+                // TODO: SET LIMIT UP FOR WINNING
+                YRoadsCoordinates.add(590);
+                YRoadsCoordinates.add(555);
+                YRoadsCoordinates.add(500);
+                YRoadsCoordinates.add(340);
+                break;
+        }
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public int getLevel() {
+        return level;
     }
    
+    
+    
 }
