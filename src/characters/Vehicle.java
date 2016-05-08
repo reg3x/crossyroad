ad1a6339b33a77d4d41be40f25f25dcb2a95b007
reg3x <diegoaguilera85@gmail.com>
2016-05-07 23:02:10 -0400

    adds method to create automatic movements for vehicles

--- src/characters/Vehicle.java
+++ src/characters/Vehicle.java
@@ -5,14 +5,55 @@
  */
 package characters;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
 /**
  *
  * @author reg3x
  */
 public class Vehicle extends GameObject {
+    String movementDirection;
+    Timer timer;
+    int xlimit;
+    
 
-    public Vehicle(String urlstring, int xInit, int yInit) {
+    public Vehicle(String urlstring, String movementDirection , int xInit, int yInit, int xlimit) {
         super(urlstring, xInit, yInit);
+        this.movementDirection = movementDirection;
+        this.xlimit = xlimit;
+        startAutomaticMovement();
+    }
+    
+    public void startAutomaticMovement(){
+        
+        //MAKE THE TIMER TIME A RANDOM NUMBER INSTEAD OF 5
+        
+        timer = new Timer(5 ,new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                switch(movementDirection){
+                    case "MoveLeft":
+                        if(getLabel().getX()>0){
+                            getLabel().setLocation(getLabel().getX()-1, getLabel().getY());
+                        }else{
+                            getLabel().setLocation(650, getyInit());
+                        }
+                        break;
+                    case "MoveRight":
+                        if(getLabel().getX()<728){
+                            getLabel().setLocation(getLabel().getX()+1, getLabel().getY());
+                        }else{
+                            getLabel().setLocation(0, getyInit());
+                        }
+                        break;
+                }
+            }
+        });
+        timer.start();
+        
     }
 
     
