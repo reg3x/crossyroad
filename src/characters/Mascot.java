5a6e551f816c7fac0dd765bbf0f0edcaf5c85d12
reg3x <diegoaguilera85@gmail.com>
2016-05-08 20:16:53 -0400

    adds mascot side icons

--- src/characters/Mascot.java
+++ src/characters/Mascot.java
@@ -5,14 +5,39 @@
  */
 package characters;
 
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author diegoaguilerazambrano
  */
 public class Mascot extends GameObject {
+    private ImageIcon leftIcon,rightIcon,upIcon, downIcon;
     
-    public Mascot(String urlstring, int xInit, int yInit) {
-        super(urlstring, xInit, yInit);
+    public Mascot(String upIconUrl, String downIconUrl,String leftIconUrl,String rightIconUrl, int xInit, int yInit) {
+        super(upIconUrl, xInit, yInit);
+        upIcon = new ImageIcon(this.getClass().getResource("/images/"+upIconUrl));
+        downIcon = new ImageIcon(this.getClass().getResource("/images/"+downIconUrl));
+        leftIcon = new ImageIcon(this.getClass().getResource("/images/"+leftIconUrl));
+        rightIcon = new ImageIcon(this.getClass().getResource("/images/"+rightIconUrl));
     }
+
+    public ImageIcon getLeftIcon() {
+        return leftIcon;
+    }
+
+    public ImageIcon getRightIcon() {
+        return rightIcon;
+    }
+
+    public ImageIcon getUpIcon() {
+        return upIcon;
+    }
+
+    public ImageIcon getDownIcon() {
+        return downIcon;
+    }
+    
+    
     
 }
